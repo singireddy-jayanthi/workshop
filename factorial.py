9b71d7fd92f487da5b1bd0260f3a85c6b73b478f
singireddy-jayanthi <jayanthisingireddy678@gmail.com>
2025-04-25 14:19:26 +0530

    all files are in local repository

---
+++ factorial.py
@@ -0,0 +1,13 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> KeyboardInterrupt
+>>> n = 6
+... 
+... # Initialize the factorial variable to 1
+... fact = 1
+... 
+... # Calculate the factorial using a for loop
+... for i in range(1, n + 1):
+...     fact *= i
+... 
+... print(fact)
